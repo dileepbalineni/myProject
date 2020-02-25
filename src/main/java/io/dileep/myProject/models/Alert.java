1fd1db746b58291596a8b86f7f73c5c3eb6e55fc
dileep balineni <dileepb@ipsgsystems.com>
2020-02-25 14:34:11 +0530

    model class account

---
+++ src/main/java/io/dileep/myProject/models/Alert.java
@@ -0,0 +1,61 @@
+package io.dileep.myProject.models;
+
+import java.util.UUID;
+
+public class Alert {
+	private String id = UUID.randomUUID().toString();
+	private String owner;
+	private String source;
+	private String type;
+	private String title;
+	private String scope;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+}
